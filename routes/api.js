3c1cbfdb88d50ac9cd323da8ccf3ab359babb7f4
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-06-02 21:13:59 +0700

    Added authorization API

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'public/uploads/' });
 const BookController = require('../controllers/API/BookController');
+const AuthController = require('../controllers/API/AuthController');
 const storeBookValidation = require('../validations/API/Book/StoreBookValidation');
+const passport = require('passport');
+require('../config/passport-api')(passport);
+
+router.post('/login', upload.any(), AuthController.login);
+router.post('/signup', upload.any(), AuthController.signUp);
+//secure route
+router.get(
+    '/profile',
+    passport.authenticate('jwt', { session: false }),
+    AuthController.profile
+);
 
 router.get('/books', BookController.getAllbooks);
 router.get('/books/:id', BookController.getBookById);
